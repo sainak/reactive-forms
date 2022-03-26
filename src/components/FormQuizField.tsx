f6aa76f113c240a13792a5bf3765cd4e86be81eb
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2022-03-26 17:15:34 +0530

    use custom multi select field

--- src/components/FormQuizField.tsx
+++ src/components/FormQuizField.tsx
@@ -1,5 +1,6 @@
 import { FormFieldType } from "../types/fieldTypes"
 import Input from "./Input"
+import MultiSelect from "./MultiSelect"
 
 interface FormInputProps extends FormFieldType {
   updateFieldValueCB?: (key: number, value: string) => void
@@ -52,7 +53,6 @@ function FormQuizField(props: FormInputProps) {
         </div>
       )
     case "select":
-    case "select-multiple":
       return (
         <select
           className="w-full rounded-lg border-2 border-gray-200 p-2"
@@ -65,11 +65,8 @@ function FormQuizField(props: FormInputProps) {
                 .join(",")
             )
           }
-          defaultValue={
-            props.type === "select-multiple" ? props.value.split(",") : props.value
-          }
+          defaultValue={props.value}
           disabled={props.updateFieldValueCB === undefined}
-          multiple={props.type === "select-multiple"}
         >
           {props.children?.map((child) => (
             <option key={child.id} value={child.id}>
@@ -77,6 +74,16 @@ function FormQuizField(props: FormInputProps) {
             </option>
           ))}
         </select>
+      )
+    case "select-multiple":
+      return (
+        <MultiSelect
+          id={props.id}
+          disabled={props.updateFieldValueCB === undefined}
+          selectedItems={props.value.split(",")}
+          items={props.children}
+          onChange={(value) => props.updateFieldValueCB?.(props.id, value)}
+        />
       )
     default:
       return <div>Unknown field type: {props.type}</div>
---
+++ src/components/MultiSelect.tsx
@@ -0,0 +1,118 @@
+import { ChevronDownIcon } from "@heroicons/react/outline"
+import { useEffect, useState } from "react"
+import { FormFieldChildType } from "../types/fieldTypes"
+
+interface MultiSelectProps {
+  id: number
+  isListOpen?: boolean
+  disabled?: boolean
+  selectedItems: string[]
+  items?: FormFieldChildType[]
+  onChange: (value: string) => void
+}
+
+export default function MultiSelect({ isListOpen = true, ...props }: MultiSelectProps) {
+  const [selectedItems, setSelectedItems] = useState(() => {
+    let items = new Set(props.selectedItems)
+    items.delete("")
+    return items
+  })
+
+  const [isListOpenState, setIsListOpen] = useState(isListOpen)
+
+  const addItem = (itemId: string) => {
+    setSelectedItems((prev) => new Set(prev).add(itemId))
+  }
+
+  const removeItem = (itemId: string) => {
+    setSelectedItems((prev) => {
+      const newSet = new Set(prev)
+      newSet.delete(itemId)
+      return newSet
+    })
+  }
+
+  const selectAll = () => {
+    setSelectedItems(() => new Set(props.items?.map((item) => item.id.toString())))
+  }
+
+  const deselectAll = () => {
+    setSelectedItems(() => new Set())
+  }
+
+  const toggleList = () => {
+    setIsListOpen(!isListOpenState)
+  }
+
+  const listItems = () =>
+    props.items?.map((item) => {
+      const itemSelected = selectedItems.has(item.id.toString())
+      return (
+        <div className="flex w-full items-center gap-2" key={item.id}>
+          <input
+            id={`id_${props}_${item.id}`}
+            type="checkbox"
+            className="rounded transition-all"
+            checked={itemSelected}
+            onChange={
+              props.disabled
+                ? undefined
+                : () =>
+                    itemSelected
+                      ? removeItem(item.id.toString())
+                      : addItem(item.id.toString())
+            }
+            disabled={props.disabled}
+          />
+          <label className="w-full cursor-pointer" htmlFor={`id_${props}_${item.id}`}>
+            {item.label}
+          </label>
+        </div>
+      )
+    })
+
+  const getTitle = () => {
+    switch (selectedItems.size) {
+      case 0:
+        return "Select items"
+      case 1:
+        return "1 item selected"
+      default:
+        return `${selectedItems.size} items selected`
+    }
+  }
+
+  useEffect(() => {
+    props.onChange([...selectedItems].join(","))
+  }, [selectedItems])
+
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={toggleList}
+        className="flex w-full items-center rounded-lg border-2 border-gray-200 p-2"
+      >
+        <div>{getTitle()}</div>
+        <ChevronDownIcon
+          className={`ml-auto h-5 w-5 transition-transform duration-300 ${
+            isListOpenState ? "rotate-180" : "rotate-0"
+          }`}
+        />
+      </button>
+      {isListOpenState && (
+        <div className="flex max-h-56 min-h-fit flex-col items-start gap-2 overflow-auto rounded-b-lg border-2 border-t-0 p-4">
+          {!props.disabled && (
+            <div
+              className="w-full cursor-pointer select-none"
+              onClick={selectedItems.size > 0 ? deselectAll : selectAll}
+            >
+              {selectedItems.size > 0 ? "Deselect all" : "Select all"}
+            </div>
+          )}
+          {listItems()}
+        </div>
+      )}
+    </div>
+  )
+}

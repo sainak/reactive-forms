f749841b64a644e56b3f0be58bff185d548762cd
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2022-04-06 17:16:18 +0530

    remove unnessary constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,3 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
-export const API_AUTH =
-  "Basic " +
-  btoa(`${import.meta.env.VITE_API_USERNAME}:${import.meta.env.VITE_API_PASSWORD}`)
+import { appendSlash } from "./apiUtils"
+
+export const API_BASE_URL = appendSlash(import.meta.env.VITE_API_BASE_URL)
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,6 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   VITE_API_BASE_URL: string
-  VITE_API_USERNAME: string
-  VITE_API_PASSWORD: string
 }
 
 interface ImportMeta {

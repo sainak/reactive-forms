d5efa19ef2eeff07643146314714307eda5d25fd
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2022-04-12 01:53:32 +0530

    added sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import * as Sentry from "@sentry/react"
+import { BrowserTracing } from "@sentry/tracing"
+import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-)
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+})
+
+const root = ReactDOM.createRoot(document.getElementById("root")!)
+root.render(<App />)
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   VITE_API_BASE_URL: string
+  VITE_SENTRY_DSN: string
 }
 
 interface ImportMeta {
